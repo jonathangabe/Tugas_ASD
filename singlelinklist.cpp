c77f0469c95770b7e25d3a0ab8cc98363417fbfe
jonathangabe <panggabean_jonathan@yahoo.co.id>
2015-09-21 22:15:00 +0700

    Update singlelinklist.cpp

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -5,13 +5,20 @@ void insertfirst(list *l, address p){
     else {
         next(p)=first(*l);
         first(*l)=p;
+    };
+
+void insertlast(list *l, address p)
+{
+    address q = first(*l);
+    while (next(q)!=nil)
+    {
+        q=next(q);
     }
+    next(q)=p;
+    next(p)=nil;
+};
 
-void insertlast(list *l, address p);
-void insertafter(address q, address p);
-
-void deletefirst(list *l,address p)
-  void deletefirst(list *l,address p){
+void deletefirst(list *l,address p){
     if(next(first(*l)==nil){
         first(*l)=nil;
        )
@@ -21,15 +28,26 @@ void deletefirst(list *l,address p)
             next(p)=nil;
         }
         dealokasi(p);
-void deletelast(list *l,address p);
-void deleteafter(address q,address *p);
+};
 
+void deletelast(list *l,address p)
+{
+    address q;
+    p=first(*l);
+    while(next(p)!=nil)
+    {
+        q=p;
+        p=next(p);
+    }
+    next(q)=nil;
+    dealokasi(p);
+};
 
 void insertafter (list *l, address p, address prec)
 {
     next(p) = next(prec);
     next(prec) = p;
-}
+};
 
 void deleteafter (list *l, address *p, address prec)
 {
@@ -37,4 +55,33 @@ void deleteafter (list *l, address *p, address prec)
     next(prec) = next(*p);
     next(*p) = nil;
     dealokasi(*p);
-}
+};
+
+address searchdata(list l, infotype x)
+{
+    address p;
+    p=first(l);
+    while(p!=nil)
+    {
+        if(strcmp(info(p).fak,x.fak)==0)
+        {
+            break;
+        }
+    }
+    return p;
+};
+
+void printinfo(list l)
+{
+    address p;
+    while(next(p)!=nil)
+    {
+        cout << "Masukan FAKULTAS : " << info(p).fak << endl;
+        cout << "Masukan JURUSAN : " << info(p).jur << endl;
+        cout << "Masukan KETUA : " << info(p).ketua << endl;
+        cout << "Masukan JUMLAH DIVISI : " << info(p).jmldiv << endl;
+        cout << "Masukan JUMLAH ANGGOTA : " << info(p).jmlang << endl;
+        p=next(p);
+        cout << endl;
+    }
+};
