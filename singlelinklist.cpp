2e599ae9e868384888e4560668cfc0e489ee8d99
jonathangabe <panggabean_jonathan@yahoo.co.id>
2015-09-21 19:41:00 +0700

    Create singlelinklist.cpp

---
+++ singlelinklist.cpp
@@ -0,0 +1,7 @@
+void insertfirst(list *l, address p);
+void insertlast(list *l, address p);
+void insertafter(address q, address p);
+
+void deletefirst(list *l,address p);
+void deletelast(list *l,address p);
+void deleteafter(address q,address *p);
