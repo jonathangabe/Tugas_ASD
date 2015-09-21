4de4d8e006ef9eb8e377d042ab53af2999024103
wahyulintang <wlinntang@gmail.com>
2015-09-21 21:25:52 +0700

    wahyu lintang
    insertafter
    deleteafter

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -5,3 +5,18 @@ void insertafter(address q, address p);
 void deletefirst(list *l,address p);
 void deletelast(list *l,address p);
 void deleteafter(address q,address *p);
+
+
+void insertafter (list *l, address p, address prec)
+{
+    next(p) = next(prec);
+    next(prec) = p;
+}
+
+void deleteafter (list *l, address *p, address prec)
+{
+    *p = next(prec);
+    next(prec) = next(*p);
+    next(*p) = nil;
+    dealokasi(*p);
+}
